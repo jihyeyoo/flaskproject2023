05e0f514999b3c4037eb777660fa978d5019fc18
유지혜 <gudong0918@gmail.com>
2023-10-26 07:57:49 +0000

    ddd

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
-from flask import Flask
-
+from flask import Flask,render_template
+import sys
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello goorm!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)
